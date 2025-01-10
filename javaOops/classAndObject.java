b9b8e719e83c4436a38131c86857b265c6bd3e1f
ABHISHEKPANDEY14E <abhishekpandey14e@gmail.com>
2025-01-10 11:22:57 +0530

    oops class started added folder named javaOops

---
+++ javaOops/classAndObject.java
@@ -0,0 +1,16 @@
+public class classAndObject {
+    public static void main(String[] args) {
+        // Todo Auto-gen
+    }
+}
+
+class car{
+    String type;
+    String brand;
+
+    public void display(){
+        System.out.println("Type: "+ type);
+        System.out.println("Brand: "+ brand);
+
+    }
+}
